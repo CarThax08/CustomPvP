24af43914167cefcdd35bd74f3e2b11dae501cf1
CarThax08 <carson.thackston@gmail.com>
2021-01-13 09:09:11 -0500

    Continued work on rankup messages.

--- src/main/java/com/github/carthax08/custompvp/Util.java
+++ src/main/java/com/github/carthax08/custompvp/Util.java
@@ -19,7 +19,7 @@ public class Util {
         int ranking = RankingMap.get(p);
         if(ranking == 0){
             return "Unranked";
-        }else if(ranking > 1 && ranking < 20){
+        }else if(ranking > 1 && ranking <= 20){
             return "Coal 1";
         }else if(ranking <= 40){
             return "Coal 2";
@@ -69,18 +69,38 @@ public class Util {
     public static void rankupPlayer(OfflinePlayer p, Integer amountToChangeBy){
         int currentRanking = RankingMap.get(p);
         int newRanking = currentRanking + amountToChangeBy;
-        if(newRanking > 20 && newRanking < 40){
-            if(p.isOnline()){
-                p.getPlayer().sendMessage("Congratulations on raking up to Coal 2!");
-            }
-        }else if(newRanking < 60){
-            p.getPlayer().sendMessage("Congratulations on raking up to Coal 3!");
-        }else if(newRanking < 80){
-            p.getPlayer().sendMessage("Congratulations on raking up to Iron 1!");
-        }else if(newRanking < 100){
-            p.getPlayer().sendMessage("Congratulations on raking up to Iron 2!");
-        }else if(newRanking < 120){
-            p.getPlayer().sendMessage("Congratulations on raking up to Iron 3!");
+        if(newRanking > currentRanking) {
+            if (newRanking > 20 && newRanking <= 40) {
+                if (p.isOnline()) {
+                    p.getPlayer().sendMessage("Congratulations on raking up to Coal 2!");
+                }
+            } else if (currentRanking < 40 && newRanking <= 60) {
+                p.getPlayer().sendMessage("Congratulations on raking up to Coal 3!");
+            } else if (newRanking <= 80) {
+                p.getPlayer().sendMessage("Congratulations on raking up to Iron 1!");
+            } else if (newRanking <= 100) {
+                p.getPlayer().sendMessage("Congratulations on raking up to Iron 2!");
+            } else if (newRanking <= 120) {
+                p.getPlayer().sendMessage("Congratulations on raking up to Iron 3!");
+            } else if (newRanking <= 140) {
+                p.getPlayer().sendMessage("Congratulations on raking up to Gold 1!");
+            } else if (newRanking <= 160) {
+                p.getPlayer().sendMessage("Congratulations on raking up to Gold 2!");
+            } else if (newRanking <= 180) {
+                p.getPlayer().sendMessage("Congratulations on raking up to Gold 3!");
+            } else if (newRanking <= 200) {
+                p.getPlayer().sendMessage("Congratulations on raking up to Diamond 1!");
+            } else if (newRanking <= 220) {
+                p.getPlayer().sendMessage("Congratulations on raking up to Diamond 2!");
+            } else if (newRanking <= 240) {
+                p.getPlayer().sendMessage("Congratulations on raking up to Diamond 3!");
+            } else if (newRanking <= 260) {
+                p.getPlayer().sendMessage("Congratulations on raking up to Emerald 1!");
+            } else if (newRanking <= 280) {
+                p.getPlayer().sendMessage("Congratulations on raking up to Emerald 2!");
+            } else if (newRanking <= 300) {
+                p.getPlayer().sendMessage("Congratulations on raking up to Emerald 3");
+            }else if (currentRanking < 300 && newRanking > 300)
         }
         //Still work in progress.
     }
